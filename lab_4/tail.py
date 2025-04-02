3cb550ee8a67dfeddab9edbe21e1cc0e7a8eb5df
DawidWyskwarski <153649273+DawidWyskwarski@users.noreply.github.com>
2025-04-02 20:12:44 +0200

    added --follow to the tail.py

--- lab_4/tail.py
+++ lab_4/tail.py
@@ -1,3 +1,5 @@
+import os
+import time
 from collections import deque
 import argparse as ap
 import sys
@@ -7,13 +9,16 @@ def tail():
 
     parser.add_argument('file', type=str, nargs='?')
     parser.add_argument('--lines','-l',  type=int, default=10)
-    #parser.add_argument('--follow', action='store_true')
+    parser.add_argument('--follow', '-f', action='store_true')
 
     args = parser.parse_args()
 
     if args.file is None:
         if not sys.stdin.isatty():
             from_stdin(args.lines)
+    elif args.follow:
+        last_pos = from_file(args.file, args.lines)
+        follow(last_pos,args.file,args.lines)
     else:
         from_file(args.file, args.lines)
 
@@ -25,8 +30,11 @@ def from_file(filename, lines):
 
         for line in file:
             stack.append(line)
+        last_pos = file.tell()
 
     stdout_stack(stack)
+
+    return last_pos
 
 def from_stdin(lines):
     stack = deque(maxlen=lines)
@@ -35,6 +43,27 @@ def from_stdin(lines):
         stack.append(line)
 
     stdout_stack(stack)
+
+def follow(pos, filename, n):
+    with open(filename, "r") as file:
+        file.seek(pos)
+
+        while True:
+            current_pos = file.tell()
+            line = file.readline()
+
+            if not line:
+                time.sleep(0.5)
+                size = os.stat(filename).st_size
+
+
+                if current_pos > size:
+                    print(f"\nfile {filename} has been shortened")
+                    file.seek(0)
+            else:
+                sys.stdout.write(line)
+                sys.stdout.flush()
+
 
 def stdout_stack(stack):
     while stack:

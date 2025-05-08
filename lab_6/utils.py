573ce5206b232c83e993c3b3640714170f4171a4
Kyloof <jankubow.sr@gmail.com>
2025-05-08 15:03:43 +0200

    bugfixes

--- lab_6/utils.py
+++ lab_6/utils.py
@@ -2,39 +2,48 @@ from datetime import datetime
 from TimeSeries import TimeSeries
 import csv
 
-def load_timeseries(csv_path, loaded_data):
-        with open(csv_path, mode="r", encoding="utf-8") as file:
-            reader = list(csv.reader(file))
 
-        station_codes = reader[1][1:]
-        measure_names = reader[2][1:]
-        avg_times = reader[3][1:]
-        units = reader[4][1:]
+def load_timeseries(csv_dict, keys, csv_path):
+    with open(csv_path, mode="r", encoding="utf-8") as file:
+        reader = list(csv.reader(file))
 
-        measurements = list(zip(*reader[6:]))
-        timestamps = measurements[0]
+    station_codes = reader[1][1:]
+    measure_names = reader[2][1:]
+    avg_times = reader[3][1:]
+    units = reader[4][1:]
 
-        for i, timestamp in enumerate(timestamps):
-            try:
-                timestamps[i] = datetime.strptime(timestamp, "%d/%m/%y %H:%M")
-            except:
-                ValueError
+    measurements = list(zip(*reader[6:]))
+    timestamps = list(measurements[0])
 
-        result = []
-        measurements = measurements[1:]
+    for i, timestamp in enumerate(timestamps):
+        try:
+            timestamps[i] = datetime.strptime(timestamp, "%d/%m/%y %H:%M")
+        except ValueError:
+            pass
 
-        #some measurements are empty, in order to not crash out on them it needs to be done
-        def safe_float(value):
-            try:
-                return float(value)
-            except ValueError:
-                return 0.0
-            
-        for i in range(len(station_codes)):
-            result.append(TimeSeries(measure_names[i], station_codes[i], avg_times[i], units[i], timestamps, list(map(safe_float,measurements[i]))))
+    result = []
+    measurements = measurements[1:]
 
-        loaded_data.extend(result)
-        return result
+    # some measurements are empty, in order to not crash out on them it needs to be done
+    def safe_float(value):
+        try:
+            return float(value)
+        except ValueError:
+            return 0.0
+
+    for i in range(len(station_codes)):
+        result.append(
+            TimeSeries(
+                measure_names[i],
+                station_codes[i],
+                avg_times[i],
+                units[i],
+                timestamps,
+                list(map(safe_float, measurements[i])),
+            )
+        )
+    mark_as_loaded(csv_dict, keys, result)
+    return result
 
 
 def mark_as_loaded(csv_dict, keys, value):
